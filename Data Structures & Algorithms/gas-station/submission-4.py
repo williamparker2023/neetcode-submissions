bbb9af88d8776b0cb6219ecc82181a19126c0210
Will Parker <137425532+williamparker2023@users.noreply.github.com>
2026-06-16 09:38:31 -0500

    Add: gas-station - submission-4

---
+++ Data Structures & Algorithms/gas-station/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        startIx = -1
+        startVal = sys.maxsize
+        cur = 0
+        n = len(gas)
+
+        for i in range(n):
+            cur += gas[i]-cost[i]
+            if cur < startVal:
+                startVal = cur
+                startIx = (i+1)%n
+        print(startIx)
+        curGas = 0
+        for i in range(startIx,n):
+            curGas += gas[i]-cost[i]
+            print(i,curGas)
+            if curGas < 0:
+                return -1
+        for i in range(0,startIx):
+            curGas += gas[i]-cost[i]
+            print(i,curGas)
+            if curGas < 0:
+                return -1
+        return startIx
