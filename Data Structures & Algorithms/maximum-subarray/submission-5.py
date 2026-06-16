a2dd0c88a33957a1306308beea6bd91ff5564f99
Will Parker <137425532+williamparker2023@users.noreply.github.com>
2026-06-16 09:50:20 -0500

    Add: maximum-subarray - submission-5

---
+++ Data Structures & Algorithms/maximum-subarray/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        pref = [nums[0]] + [0]*(n-1)
+
+        for i in range(1,n):
+            pref[i] = pref[i-1] + nums[i]
+    
+        smallestBefore =[0] + [pref[0]] + [0]*(n-2)
+        for i in range(1,n):
+            smallestBefore[i] = min(smallestBefore[i-1],pref[i-1])
+
+        ans = -sys.maxsize
+        for i in range(n):
+            ans = max(ans, pref[i]-smallestBefore[i])
+        
+        print(pref)
+
+        return ans
