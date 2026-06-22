f789d723ef7b6ba40bda8a3e24a758d178ef9b0d
Will Parker <137425532+williamparker2023@users.noreply.github.com>
2026-06-22 09:20:24 -0500

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.py
@@ -0,0 +1,34 @@
+class TimeMap:
+
+    def __init__(self):
+        self.dic = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.dic:
+            self.dic[key] = []
+        self.dic[key].append([timestamp,value])
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.dic:
+            return ""
+        print(self.dic)
+        n = len(self.dic[key])
+        l = 0
+        r = n-1
+        m = (l+r)//2
+        last = [-1,""]
+
+        while l<=r:
+            print(l,r,m)
+            print(last)
+            t,k = self.dic[key][m]
+            if t<=timestamp and t>=last[0]:
+                last = [t,k]
+            if t<=last[0]:
+                l = m+1
+            else:
+                r = m-1
+            m = (l+r)//2
+            print(l,r,m)
+            print(last)
+        return last[1]
